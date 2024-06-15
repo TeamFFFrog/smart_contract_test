190c9f94be01c4808773e90bb7254ed676d67b6b
Zefeng Pei <117087482+SaysZEN@users.noreply.github.com>
2024-06-15 15:01:54 +0800

    Add files via upload

--- REG/script/deployProxy.s.sol
+++ REG/script/deployProxy.s.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "../src/MyToken.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "forge-std/Script.sol";
+
+contract DeployUUPSProxy is Script {
+    function run() public {
+
+        address _implementation = 0x33Bd3D49C3B2B1d4fCC74d23977F34dbccfAeDf3; // Replace with your token address
+        vm.startBroadcast();
+
+        // Encode the initializer function call
+        bytes memory data = abi.encodeWithSelector(
+            MyToken(_implementation).initialize.selector,
+            msg.sender // Initial owner/admin of the contract
+        );
+
+        // Deploy the proxy contract with the implementation address and initializer
+        ERC1967Proxy proxy = new ERC1967Proxy(_implementation, data);
+
+        vm.stopBroadcast();
+        // Log the proxy address
+        console.log("UUPS Proxy Address:", address(proxy));
+    }
+}
