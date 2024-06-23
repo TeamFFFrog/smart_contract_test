b5e453f38f8147eafff99ea22b9ec10cc1cf4c16
Zefeng Pei <117087482+SaysZEN@users.noreply.github.com>
2024-06-23 14:29:40 +0800

    Add files via upload

--- src/TokenDistributor.sol
+++ src/TokenDistributor.sol
@@ -3,22 +3,19 @@ pragma solidity ^0.8.0;
 
 import "./RegToken.sol";
 import "./FishToken.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TokenOperations {
+contract TokenOperations is Ownable {
     RegToken public regToken;
     FishToken public fishToken;
-    address public owner;
 
-    constructor(address _regTokenAddress, address _fishTokenAddress) {
+    constructor(
+        address _regTokenAddress,
+        address _fishTokenAddress,
+        address _owner
+    ) Ownable(_owner) {
         regToken = RegToken(payable(_regTokenAddress));
         fishToken = FishToken(_fishTokenAddress);
-        owner = msg.sender;
-    }
-
-    // Modifier: 只有当前 owner 才能调用
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can perform this action");
-        _;
     }
 
     // 质押1个 REG 代币到合约
@@ -27,7 +24,7 @@ contract TokenOperations {
         regToken.transferFrom(msg.sender, address(this), 1);
     }
 
-    // 归还1个 REG 代币并赠送300个 FISH 代币
+    // 归还1个 REG 代币并赠送300个 FISH 代币，只有 owner 可调用
     function returnRegAndReward(address recipient) external onlyOwner {
         require(regToken.balanceOf(address(this)) >= 1, "Contract has insufficient REG balance");
         
@@ -35,8 +32,8 @@ contract TokenOperations {
         fishToken.mintOrReward(recipient, 300);
     }
 
-    // 销毁质押的 REG 代币
-    function burnReg() external onlyOwner {
+    // 销毁质押的 REG 代币，只有 owner 可调用
+    function burnStakedReg() external onlyOwner {
         uint256 stakedBalance = regToken.balanceOf(address(this));
         require(stakedBalance >= 3, "Insufficient staked REG balance");
 
@@ -45,24 +42,10 @@ contract TokenOperations {
         }
     }
 
-    // 转移合约的 owner
-    function transferOwnership(address newOwner) external onlyOwner {
+    // 转移合约的 owner，只有当前 owner 可调用
+    function transferOwnership(address newOwner) public override onlyOwner {
         require(newOwner != address(0), "New owner address cannot be zero address");
-        owner = newOwner;
-    }
-
-
-
-    // 销毁质押的 REG 代币
-    function burnStakedReg() external {
-        require(msg.sender == owner, "Only owner can perform this action");
-        uint256 stakedBalance = regToken.balanceOf(address(this));
-        require(stakedBalance >= 3, "Insufficient staked REG balance");
-
-        for (uint256 i = 0; i < 3; i++) {
-            regToken.burn(address(this), 1);
-        }
+        super.transferOwnership(newOwner);
     }
 }
-
 
